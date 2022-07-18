4c608499d45968b74130ffa111006bbe9f014efd
Benjamin <muhlestein1@gmail.com>
2022-07-18 16:13:00 -0700

    Adding home page view

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use('/places', require('./controllers/places'))
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads!')
+  res.render('home')
 })
 
 // LISTEN
-app.listen(PORT, () => {
-  console.log('nomming at port', PORT);
+app.listen(process.env.PORT)
 
-})
+
+
+
+
+
+
+
+
+
 
