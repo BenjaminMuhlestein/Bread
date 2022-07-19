9f05beb28e3fb83a6995f9e78df7a28ab3b3b240
Benjamin <muhlestein1@gmail.com>
2022-07-19 09:51:38 -0700

    Adding styles to REST-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -1,18 +1,36 @@
 const React = require('react')
 const Def = require('./default')
+const React = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+                /* BOOTSTRAP CDN LINK (long) */
+                <link rel="stylesheet" href="/css/style.css"/>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = Def
 
 function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>HOME111</h1>
                 <div>
-                  <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                  <img src="images/drink.jpg" alt="Chia Fruit Shake" />
                 </div>
                 <div>Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                 </div>
                 <a href="/places">
-                    <button className="btn-primary">Places Page</button>
+                    <button className="btn-primary">Places Page@</button>
                 </a>
             </main>
         </Def>
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,6 +1,29 @@
 const React = require('react')
 const Def = require('../default')
+  // Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+// Controllers & Routes
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('*', (req, res) => {
+    res.render('error404')
+})
+
+// Listen for Connections
+app.listen(process.env.PORT)
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
@@ -31,6 +54,8 @@ function index (data) {
       </Def>
     )
   }
+
+
 
 module.exports = index
   
