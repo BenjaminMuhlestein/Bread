1f3eb2b4200312d8a6897f3471db40febcdabbe2
Benjamin <muhlestein1@gmail.com>
2022-07-18 17:49:21 -0700

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,45 @@
-// GET /places
-app.get('/', (req, res) => {
-    let places = []
-    res.render('places/index')
-  })
-  let places = [{
+let places = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
+    pic: '/images/h-thai-ml-tables.png'
   }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: '/images/coffee-cat.jpg'
   }]
+
+  function index (data) {
+    let placesFormatted = data.places.map((place) => {
+        return (
+          <div className="col-sm-6">
+            <h2>{place.name}</h2>
+            <p clasName="text-center">
+              {place.cuisines}
+            </p>
+            <img src={place.pic} alt={place.name} />
+            <p clasName="text-center">
+              Located in {place.city}, {place.state}
+            </p>
+          </div>
+        )
+      })
+      
+    return (
+      <Def>
+          <main>
+              <h1>Places to Rant or Rave About</h1>
+              <div className="row">
+                {placesFormatted}
+              </div>
+          </main>
+      </Def>
+    )
+  }
+  
+  
   
   
--- views/home.jsx
+++ views/home.jsx
@@ -1,38 +1,23 @@
 const React = require('react')
 const Def = require('./default')
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-                /* BOOTSTRAP CDN LINK (long) */
-                <link rel="stylesheet" href="/css/style.css"/>
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
-    )
-}
-
-module.exports = Def
-
-
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-              <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
-
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
+                </div>
+                <div>Photo by <a href="https://unsplash.com/@cravethebenefits?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Brenda Godinez</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+                <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
 
