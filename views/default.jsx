7db323da2a134142dfae02030f29c41b3f3b8347
Benjamin <muhlestein1@gmail.com>
2022-07-20 19:02:15 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -3,17 +3,31 @@ const React = require('react')
 function Def (html) {
     return (
         <html>
-            <head>
-                <title>Title</title>
-            </head>
+            <head>...</head>
             <body>
+            <nav>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/places">Places</a>
+    </li>
+    <li>
+      <a href="/places/new">Add Place</a>
+    </li>
+  </ul>
+</nav>
+
+
+
                 {html.children}
             </body>
         </html>
     )
-  }
-  
+}
 
 module.exports = Def
+
 
 
