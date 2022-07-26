f6e6bbf66f0b8d893957e655d93dc8dbf4eb09ca
Benjamin <muhlestein1@gmail.com>
2022-07-26 13:01:55 -0700

    added new code for part 9

--- models/places.js
+++ models/places.js
@@ -1,52 +1,44 @@
-const router = require('express').Router()
-const db = require('../models')
+const mongoose = require('mongoose')
 
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: { type: String, default: 'http://placekitten.com/350/350' },
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: {
+    type: Number,
+    min: [1673, 'Surely not that old?!'],
+    max: [new Date().getFullYear(), 'Hey, this year is in the future!']
+  }
+})
+
+
+placeSchema.methods.showEstablished = function() {
+  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
+
+module.exports = mongoose.model('Place', placeSchema)
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
       res.redirect('/places')
   })
   .catch(err => {
-      console.log('err', err)
+    if (err && err.name == 'ValidationError') {
+      let message = 'Validation Error: '
+      for (var field in err.errors) {
+          message += `${field} was ${err.errors[field].value}. `
+          message += `${err.errors[field].message}`
+      }
+      console.log('Validation error message', message)
+      res.render('places/new', { message })
+  }
+  else {
       res.render('error404')
+  }
+  
   })
 })
 
 
-router.get('/', (req, res) => {
-  res.send('GET /places stub')
-})
-
-router.post('/', (req, res) => {
-  res.send('POST /places stub')
-})
-
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
-
-router.get('/:id', (req, res) => {
-  res.send('GET /places/:id stub')
-})
-
-router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
-})
-
-router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
-})
-
-router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
-})
-
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
-})
-
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
-})
-
-module.exports = router
