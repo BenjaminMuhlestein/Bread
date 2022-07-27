03e8c0aab0c83e2635d9eb3432e19ebf4c50a043
Benjamin <muhlestein1@gmail.com>
2022-07-26 23:11:33 -0700

    added part 10 code

--- views/show.jsx
+++ views/show.jsx
@@ -1,44 +1,68 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form (data) {
-    let message = ''
-    if (data.message) {
-        message = (
-            <h4 className="alert-danger">
-              {data.message}
-            </h4>
+function show (data) {
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+    let rating = (
+        <h3 className="inactive">
+          Not yet rated
+        </h3>
+      )
+      if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+          return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+          stars += 'â­ï¸'
+        }
+        rating = (
+          <h3>
+            {stars} stars
+          </h3>
         )
-
-    }
-
+       
+      }
+      
+      if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+          return (
+            <div className="border col-sm-4">
+              ...
+              <form method="POST" action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+                <input type="submit" className="btn btn-danger" value="Delete Comment" />
+              </form>
+            </div>
+          )
+        })
+      }
+      
+      
     return (
         <Def>
-    <main>
-    <div className="row"> </div>
-      <div className="col-sm-6">
-        <img src={data.place.pic} alt={data.place.name} />
-        <h3>
-          Located in {data.place.city}, {data.place.state}
-        </h3>
-      </div>
-      <div className="col-sm-6">
-        ...
-        <h2>
-          Description
-        </h2>
-        <h3>
-          {data.place.showEstablished()}
-        </h3>
-        <h4>
-          Serving {data.place.cuisines}
-        </h4>
-        ...
-    </div>
-  </main>
+          <main>
+            <div className="row">
+              ...
+              <div className="col-sm-6">
+                <h1>{ data.place.name }</h1>
+                <h2>
+                  Rating
+                </h2>
+                {rating}
+                <br />
+                ...
+              </div>
+            </div>
+          </main>
         </Def>
-    )
-}
+      )
+    }
+
 
 module.exports = show
 
