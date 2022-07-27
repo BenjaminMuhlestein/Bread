c986772b75d2fcf50cb796e1a3915b43b5a97ea6
Benjamin <muhlestein1@gmail.com>
2022-07-26 22:13:13 -0700

    added code for part 10

--- controllers/places.js
+++ controllers/places.js
@@ -16,10 +16,20 @@ let places = [{
   router.get('/',(req, res) => {
     res.render('places/index', {places})
   })
-  
-  router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
+  router.get('/:id', (req, res) => {
+    db.Place.findById(req.params.id)
+    .populate('comments')
+    .then(place => {
+        console.log(place.comments)
+        res.render('places/show', { place })
+    })
+    .catch(err => {
+        console.log('err', err)
+        res.render('error404')
+    })
+})
+
+
   
   //Sending and Receiving Your Form Data
   router.post('/', (req, res) => {
@@ -67,6 +77,27 @@ router.put('/:id', (req, res) => {
       places[id] = req.body
       res.redirect(`/places/${id}`)
   }
+})
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
 
